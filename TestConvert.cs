cc213c8c53cd39a5bebd188f5d0248eec6408e25
Pham Duy Han <han.phamduy@vmedgroup.io>
2021-08-05 15:16:45 +0700

    test

---
+++ TestConvert.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TestConverter
+{
+    private JToken ConvertHisObjectByJsonObject(JObject jObject, JToken hisObject)
+    {
+        //Nếu type là array
+        if (hisObject.Type == JTokenType.Array)
+        {
+            var result = new JArray();
+            foreach (var itemArr in hisObject)
+            {
+                var newItem = ConvertHisObjectByJsonObject(jObject, itemArr);
+                result.Add(newItem);
+            }
+
+            return result;
+        }
+        //Nếu type là object
+        else if (hisObject.Type == JTokenType.Object)
+        {
+            var hisObjectConverted = (JObject)hisObject;
+            var result = new JObject(hisObjectConverted);
+            foreach (var item in hisObjectConverted.Properties())
+            {
+                var newProperty = jObject.Properties().FirstOrDefault(i => i.Value.ToString() == item.Name);
+                if (newProperty != null)
+                {
+                    result.Remove(item.Name.ToString());
+                    result.Add(newProperty.Name, ConvertHisObjectByJsonObject(jObject, item.Value));
+                }
+            }
+
+            return result;
+        }
+        else
+        {
+            return hisObject;
+        }
+    }
+
+    /// <summary>
+    /// Hàm convert từ json Object của His sang json Object của emr
+    /// </summary>
+    /// <param name="hisObject"></param>
+    /// <param name="pluginName"></param>
+    /// <returns></returns>
+    public JToken ConvertHisObjectToEmrObject(JToken hisObject)
+    {
+        try
+        {
+            var jsonObject = new JObject();
+
+            using (StreamReader file = File.OpenText(@"./plugins/thongtingiaychuyenvien/Converter.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                jsonObject = (JObject)serializer.Deserialize(file, typeof(JObject));
+            }
+
+            var result = ConvertHisObjectByJsonObject(jsonObject, hisObject);
+
+            return result;
+        }
+        catch(Exception ex)
+        {
+            throw new Exception("Lỗi ", ex);
+        }
+    }
+
+}
---
+++ Tranform.cs
@@ -0,0 +1,39 @@
+﻿using Emr.Pluggable.Interface;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Converter;
+namespace Emr.Plugins.Tranform.Test.LayThongTinChuyenVien
+{
+    public class Tranform : ITransformPlugin
+    {
+        public string Name
+        {
+            get { return "LayThongTinChuyenVien"; }
+        }
+
+        public string Version
+        {
+            get { return "1.0.0.0"; }
+        }
+
+        JToken ITransformPlugin.Tranform(JToken data, Dictionary<string, object> requestParams)
+        {
+            try
+            {
+                var convert = new EmrConverter();
+                var resultData = convert.ConvertHisObjectToEmrObject(data);
+
+                return resultData;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Có lỗi xãy ra, vui lòng liên hệ Quản trị viên!.", ex);
+            }
+        }
+    }
+}
